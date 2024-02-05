f670f5002a669a0038cbcd49031aa96dcaf6b73f
Yahya Ömer Kaya <yahya.7373@gmail.com>
2024-02-05 10:24:37 +0300

    disabled auto indent on move

--- lua/yahya/init.lua
+++ lua/yahya/init.lua
@@ -26,8 +26,8 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>q", vim.cmd.Ex)
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv")
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
