ea8087162e9f68d9d27f2060cd74dfcda83fe5f2
Yahya Ömer Kaya <yahya.7373@gmail.com>
2024-02-20 20:39:47 +0300

    code action keymap

--- lua/yahya/init.lua
+++ lua/yahya/init.lua
@@ -53,6 +53,7 @@ vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
 vim.keymap.set("n", "gd", vim.lsp.buf.definition)
 vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
 vim.keymap.set("n", "gr", vim.lsp.buf.references)
+vim.keymap.set("n", "gf", vim.lsp.buf.code_action)
 vim.keymap.set("n", "K", vim.lsp.buf.hover)
 
 vim.keymap.set("n", "<A-d>", ":%s/\\<<C-r><C-w>\\>//gI<Left><Left><Left>")
