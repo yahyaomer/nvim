0956728ee8fbda26d86b7b0bd70fa9cb5aa76328
Yahya Ömer Kaya <yahya.7373@gmail.com>
2024-03-13 16:57:55 +0300

    search zz update

--- lua/yahya/init.lua
+++ lua/yahya/init.lua
@@ -33,8 +33,6 @@ vim.keymap.set("v", ">", ">gv")
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
 
 vim.keymap.set("n", "<A-u>", "<C-w><")
 vim.keymap.set("n", "<A-i>", "<C-w>+")
