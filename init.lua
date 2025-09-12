c5b8ba992428095f174720b390a47f8195a67948
Yahya Ömer Kaya <yahya.7373@gmail.com>
2025-09-12 11:38:04 +0300

    neovide opacity

--- init.lua
+++ init.lua
@@ -202,6 +202,7 @@ require("lazy").setup(plugins, {})
 vim.cmd.colorscheme("onedark")
 
 vim.o.guifont = "JetBrainsMonoNL NF:h11"
-vim.keymap.set("n", "<C-->", function () vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1 end)
-vim.keymap.set("n", "<C-=>", function () vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1 end)
-vim.keymap.set("n", "<C-0>", function () vim.g.neovide_scale_factor = 1.0 end)
+vim.g.neovide_opacity = 0.9
+vim.keymap.set("n", "<C-->", function() vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1 end)
+vim.keymap.set("n", "<C-=>", function() vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1 end)
+vim.keymap.set("n", "<C-0>", function() vim.g.neovide_scale_factor = 1.0 end)
